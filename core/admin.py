f563fbddf8368d23ea292a18826475ef2d57df77
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2025-12-24 20:17:51 +0600

    Add User model, serializer, and update admin registration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Course, Lesson,Enrollment,QuestionAnswer, Material
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Enrollment)
+admin.site.register(QuestionAnswer)
+admin.site.register(Material)
