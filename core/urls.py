9e2e06f2e5cae89d19178704f5ed31b1139c19f1
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2025-12-26 11:29:23 +0600

    Fix URL patterns by ensuring lesson list creation endpoint is included

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('category/', views.category_list_create, name='category_list_create'),
-    path('course/', views.course_list_create, name='course_list_create')
+    path('course/', views.course_list_create, name='course_list_create'),
+    path('lesson/', views.lesson_list_create, name='lesson_list_create')
 ]
